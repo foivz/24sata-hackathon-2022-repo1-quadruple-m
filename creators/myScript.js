33d02df91a9ebd61e5e377ee71df7d80e694466a
Matej Drenjančević <matej.drenjancevic@gmail.com>
2022-04-02 10:19:17 +0200

    changed .gitignore and added myScript.js

---
+++ creators/myScript.js
@@ -0,0 +1,11 @@
+const crypto = require("crypto");
+const {fax_secret} = require("../config.json")
+const argon2 = require("argon2");
+const {validateEmail, validatePassword} = require("../utils/validatorUtils");
+
+
+module.exports = api => {
+    
+
+
+}
